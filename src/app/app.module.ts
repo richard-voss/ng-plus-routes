8d3d4e68769f08877f811377a7ca99884be41c2f
Richard Voß <richard.voss@wps.de>
2018-09-19 21:10:40 +0200

    replace wise-person with random-image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { RandomStatementComponent } from './random-statement/random-statement.component';
 import { RandomGifComponent } from './random-gif/random-gif.component';
 import { SafePipe } from './safe.pipe';
-import { WisePersonComponent } from './wise-person/wise-person.component';
-import { RandomImageComponent } from './landscape/random-image.component';
+import { RandomImageComponent } from './random-image/random-image.component';
 
 
 const adviceChildren: Routes = [
-  { path: 'person', component: WisePersonComponent },
+  {
+    path: 'person', component: RandomImageComponent, data: {
+      images: [
+        'https://wisewomen.com.au/wp-content/uploads/2015/10/WW-Marie-Curie.jpg',
+        'https://wisewomen.com.au/wp-content/uploads/2018/01/WW-Angela-Merkel.jpg',
+        'https://memegenerator.net/img/images/300x300/10318247/mr-ts-shakespearean-wisdom.jpg',
+        'https://memegenerator.net/img/images/300x300/11776471/wisdom-dropping-confucius.jpg',
+        'https://memegenerator.net/img/images/300x300/16037641/wisdom-of-yoda.jpg'
+      ]
+    }
+  },
   {
     path: 'landscape', component: RandomImageComponent, data: {
       images: [
@@ -31,7 +40,6 @@ const adviceChildren: Routes = [
     RandomStatementComponent,
     RandomGifComponent,
     SafePipe,
-    WisePersonComponent,
     RandomImageComponent
   ],
   imports: [
